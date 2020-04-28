d9715e982b1c35eb0bfb7cc162700f884bc4253d
Tuan Anh <anhntd00558@fpt.edu.vn>
2020-04-28 17:51:30 +0700

    Form Feedback version 1

--- Test_Project_IdentityMVC/Models/IdentityModels.cs
+++ Test_Project_IdentityMVC/Models/IdentityModels.cs
@@ -11,6 +11,7 @@ namespace Test_Project_IdentityMVC.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
         [Display(Name = "Last Name")]
@@ -56,6 +57,7 @@ namespace Test_Project_IdentityMVC.Models
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
+        public DbSet<Feedback> Feedbacks { get; set; }
 
         public static ApplicationDbContext Create()
         {
